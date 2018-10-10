400caa0aa75592eb9ce3983504324c7efbbc9b84
Gregor Corbin <corbin@mathematik.uni-kl.de>
2018-10-10 19:01:40 +0200

    removed old scripts read project options into a dictionary first

--- projectoptions.py
+++ projectoptions.py
@@ -4,11 +4,26 @@ import os
 class ProjectOptions:
 
     def __init__(self, optionsFileName):
-        self._config = configparser.ConfigParser(interpolation = configparser.ExtendedInterpolation())
-        self._config.read(optionsFileName) 
+        config = configparser.ConfigParser(interpolation = configparser.ExtendedInterpolation())
+        config.read(optionsFileName) 
+        
+        self._options = {}
+        self._options['do-git-checkout'] = config.getboolean('options', 'do-git-checkout', fallback=False)
+        self._options['do-build'] = config.getboolean('options', 'do-build', fallback=False)
+        if self._options['do-build']:
+            self._options['build-command'] = config.get('options','build-command')
+        else:
+            self._options['build-command'] = ''
+        self._options['hash-algorithm'] = config.get('options','hash-algorithm',fallback='sha256')
+        
+        self._paths = {}
+        for key in ['git-path', 'top-path', 'build-path', 'input-data-path',
+                    'output-data-path', 'parameter-list', 'last-command', 
+                    'get-input-files']:
+            self._paths[key] = config.get('paths',key)
 
     def path(self,key):
-        return self._config['paths'][key]
+        return self._paths[key]
 
     def option(self,key):
-        return self._config['options'][key]
+        return self._options[key]
