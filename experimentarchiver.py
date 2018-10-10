0538d4bdf5247fefdb05a5beff51304815e65720
Gregor Corbin <corbin@mathematik.uni-kl.de>
2018-10-10 16:13:57 +0200

    work in progress: experimentState

--- experimentarchiver.py
+++ experimentarchiver.py
@@ -14,11 +14,6 @@ def readListFromFile(filename):
         resultList = [line.strip() for line in f]
     return resultList
 
-def readArgumentsFromFile(filename):
-    args = []
-    with open(filename,'r') as f:
-        args = f.readline().strip('\n').strip(' ').split(' ')
-    return args
 
 def getPathsToInputData(pathToScriptFile):
     importedModule = imp.load_source('tmp_script_module',pathToScriptFile)
@@ -32,9 +27,11 @@ def getPathsToInputData(pathToScriptFile):
         os.chdir(savePath)
     return pathList
 
+
 def make_directory_if_nonexistent(path):
     if not os.path.isdir(path):
         os.mkdir(path)
+
 
 def split_all_subpaths(path):
     subs = []
@@ -45,21 +42,36 @@ def split_all_subpaths(path):
             break
         path = parts[0]
     return subs
-        
+
+
 def make_all_directories(path):
     if os.path.isabs(path):
         raise Exception('Only relative paths allowed')
     for sub in split_all_subpaths(path):
         make_directory_if_nonexistent(sub)
-        
+
+
 def read_json(filename):
     with open(filename,'r') as f:
         data = json.load(f)
     return data
 
+
 def write_json(data,filename):
     with open(filename,'w') as f:
         json.dump(data,f)
+
+
+def copy_files(fromPath,toPath,files,createDirectories=False):
+    for relPath in files:
+        if os.path.isabs(relPath):
+            raise Exception('Only relative paths allowed')
+        fromFile = os.path.join(fromPath,relPath)
+        toFile = os.path.join(toPath,path)
+        if createDirectories: 
+            make_all_directories(os.path.dirname(toFile))
+        shutil.copy2(fromFile, toFile)
+
 
 class ExperimentState:
     def __init__(self, projectOptions,archiveOptions):
@@ -82,38 +94,44 @@ class ExperimentState:
             data.append((relPath, hashAlgorithm, fileHash))
         return data
 
-    
-    #def read_stored_input_data(self):
-        #data = []
-        #with open(self._archiveOptions.path('input-files'),'r') as f:
-            #data = json.load(f)
-        ##todo: check data for correctness
-        #return data
-            
-    #def store_input_data(self,data):
-        #with open(self._archiveOptions.path('input-files'),'w') as f:
-            #json.dump(data,f)
-            
-            
     def verify_hashes(self):
         for (relPath, hashAlgorithm, storedHash) in self._inputData:
             absPath = os.path.join(self._projectOptions.path('input-data-path'),relPath)
             computedHash = project_utils.computeFileHash(absPath,hashAlgorithm)
             if (storedHash != computedHash):
                 raise Exception("The stored and computed {0} hashes for the file {1} differ: {2} != {3}".format(hashAlgorithm,absPath,storedHash,computedHash))
-        
-            
+
+    def _build_project(self):
+        buildCommand = self._projectOptions.option('build-command').strip().split()
+        savePath = os.getcwd()
+        try:
+            os.chdir(self._projectOptions.path('build-path'))
+            check_call(buildCommand)
+        finally:
+            os.chdir(savePath)
 
     def read_from_project(self):
         self._commitHash = git_utils.getGitCommitHash(self._projectOptions.path('git-path'))
         self._pathsToParameters = readListFromFile(self._projectOptions.path('parameter-list'))
         self._inputData = self.hash_input_data()
         self._pathsToOutputData = [] # not yet implemented
-        self._command = readArgumentsFromFile(self._projectOptions.path('last-command'))
+        self._command = read_json(self._projectOptions.path('last-command'))
         self._environment = None # not yet implemented
-        
+
+
     def restore_to_project(self):
-        pass
+        git_utils.checkoutGitCommit(self._projectOptions.path('git'),self._commitHash)
+        copy_files(self._archiveOptions.path('parameter-path'),
+                   self._projectOptions.path('top-path'),
+                   self._pathsToParameters,
+                   createDirectories=False)
+        # nothing to do for input data
+        # output data not implemented
+        write_json(self._command, self._projectOptions.path('last-command'))
+        # environment not implemented 
+        if self._projectOptions.option('do-build'):
+            self._build_project()
+
 
     def read_from_archive(self):
         self._commitHash = git_utils.readCommitHashFromFile(self._archiveOptions.path('commit-hash'))
@@ -121,26 +139,27 @@ class ExperimentState:
         self._inputData = read_json(self._archiveOptions.path('input-files')) 
         self.verify_hashes()
         self._pathsToOutputData = [] # not yet implemented
-        self._command = readArgumentsFromFile(self._archiveOptions.path('last-command'))
+        self._command = read_json(self._archiveOptions.path('last-command'))
         self._environment = None # not yet implemented
-    
+
+
     def write_to_archive(self):
         make_directory_if_nonexistent(self._archiveOptions.path('archive-path'))
         make_directory_if_nonexistent(self._archiveOptions.path('experiment-path'))
         git_utils.writeCommitHashToFile(self._archiveOptions.path('commit-hash'),self._commitHash) 
         make_directory_if_nonexistent(self._archiveOptions.path('parameter-path'))
         write_json(self._pathsToParameters,self._archiveOptions.path('parameter-list'))
-        for path in self._pathsToParameters:
-            fromFile = os.path.join(self._projectOptions.path('top-path'),path)
-            toFile = os.path.join(self._archiveOptions.path('parameter-path'),
-                                          path)
-            make_all_directories(os.path.dirname(toFile))
-            shutil.copy2(fromFile, toFile)
+        copy_files(self._projectOptions.path('top-path'),
+                   self._archiveOptions.path('parameter-path'),
+                   self._pathsToParameters,
+                   createDirectories=True)
         write_json(self._inputData,self._archiveOptions.path('input-files'))
         # output data not yet implemented
         write_json(self._command,self._archiveOptions.path('last-command'))
         #environment not yet implemented
-        
+
+
+
 class ExperimentArchiver:
     
     def __init__(self, archiverOptions, projectOptions):
